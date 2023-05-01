482817f7409889501ca4cb7a5eaac3957dc56438
Emma Gerigscott <elgerig@gmail.com>
2023-05-01 13:57:54 -0700

    add more questions and check question methods
    Co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- Sphinx.Tests/ModelTests/SphinxerTests.cs
+++ Sphinx.Tests/ModelTests/SphinxerTests.cs
@@ -15,15 +15,6 @@ namespace Sphinx.Tests
       Assert.AreEqual(typeof(Riddle), newRiddle.GetType());
     }
 
-    // [TestMethod]
-    // public void Riddles_HasKeyOfRiddleAndValueOfAnswer_String()
-    // {
-    //   Riddle newRiddle = new Riddle();
-    //   Dictionary<string, string> expected = new Dictionary<string, string> {{ "kyle", "what is probably 6 feet tall and definitely stupid"}, {"a bottle", "what best describes the shape of kyle's head?"}, {"kyle's shirt", "what is sweaty and probably grey?"}};
-    //   Dictionary<string, string> result = newRiddle.Riddles;
-    //   CollectionAssert.AreEqual(expected, result);
-    // }
-
     [TestMethod]
     public void Question1_GetsQuestion1_String()
     {
--- Sphinx/Models/Sphinxer.cs
+++ Sphinx/Models/Sphinxer.cs
@@ -4,11 +4,65 @@ namespace Sphinx.Models
 {
   public class Riddle
   {
+    private string _easterEgg1 = "IDK I'm not Kyle";
+    private string _easterEgg2 = "Who is Kyle?";
     public string Question1 { get; } = "what is Kyle's middle name?";
     public bool CheckQuestion1(string input)
     {
       string answer = "joseph";
-      if (input == answer)
+      if (input == answer || input == _easterEgg1 || input == _easterEgg2)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public string Question2 { get; } = "What year was Kyle born?";
+    public bool CheckQuestion2(string input)
+    {
+      string answer = "1924";
+      if (input == answer || input == _easterEgg1 || input == _easterEgg2)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public string Question3 { get; } = "What instrument does Kyle play?";
+    public bool CheckQuestion3(string input)
+    {
+      string answer = "nose flute";
+      if (input == answer || input == _easterEgg1 || input == _easterEgg2)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public string Question4 { get; } = "What is Kyle's favorite hobby?";
+    public bool CheckQuestion4(string input)
+    {
+      string answer = "singing";
+      if (input == answer || input == _easterEgg1 || input == _easterEgg2)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public string Question5 { get; } = "What is Kyle's favorite TV show?";
+    public bool CheckQuestion5(string input)
+    {
+      string answer = "jeopardy";
+      if (input == answer || input == _easterEgg1 || input == _easterEgg2)
       {
         return true;
       }
